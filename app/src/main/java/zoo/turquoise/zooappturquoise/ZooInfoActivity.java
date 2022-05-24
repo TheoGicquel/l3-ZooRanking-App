a23908f25c7ae743cc4685e4b37245fec474d327
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-05-24 18:44:29 +0200

    added code provided by alix

---
+++ app/src/main/java/zoo/turquoise/zooappturquoise/ZooInfoActivity.java
@@ -0,0 +1,16 @@
+package zoo.turquoise.zooappturquoise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class ZooInfoActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.zoo_info_layout);
+        }
+    }
+
