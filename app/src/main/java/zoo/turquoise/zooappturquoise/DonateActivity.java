3d0c8ea1ab5d6d475d7bc89629270efbe2174001
Théo Gicquel <theo.gicquel.work@gmail.com>
2022-05-24 18:02:47 +0200

    finished main menu navigation

---
+++ app/src/main/java/zoo/turquoise/zooappturquoise/DonateActivity.java
@@ -0,0 +1,21 @@
+package zoo.turquoise.zooappturquoise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DonateActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.donate_layout);
+    }
+
+    public void switchToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
